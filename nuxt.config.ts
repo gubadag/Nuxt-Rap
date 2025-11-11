732911b0fd47d1060aa539c71e14fe992027a72d
Sahy <kakajansahy@gmail.com>
2025-11-12 00:05:05 +0200

    resolve index 404

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   ssr: true,
-  nitro: { preset: 'node-server' },
+  nitro: { preset: 'netlify' },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
